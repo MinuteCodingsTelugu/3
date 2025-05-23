5c3c3aed21b23f0a00216854c3d6da3250d43fe2
MinuteCodingsTelugu <156009168+MinuteCodingsTelugu@users.noreply.github.com>
2025-05-23 20:20:01 +0530

    Update config.py

--- functions/config.py
+++ functions/config.py
@@ -21,48 +21,36 @@ from decouple import config
 
 class Var:
     # Version
-
-    __version__ = "v0.0.8"
+    version = "v0.0.8"
 
     # Telegram Credentials
-
     API_ID = config("API_ID", default=16978078, cast=int)
     API_HASH = config("API_HASH", default="91ccaf748f031b656bbf64ff47f990e3")
     BOT_TOKEN = config("BOT_TOKEN", default="8155621239:AAGZF_wN_jXkK3fEOgzCHM5a19SUVFhLf4s")
     SESSION = config("SESSION", default=None)
 
     # Database Credentials
-
     MONGO_SRV = config("MONGO_SRV", default="mongodb+srv://autoanime:autoanime@cluster0.iimy622.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
     # Channels Ids
-
     BACKUP_CHANNEL = config("BACKUP_CHANNEL", default=-1001945054800, cast=int)
     MAIN_CHANNEL = config("MAIN_CHANNEL", default=-1002335038787, cast=int)
     LOG_CHANNEL = config("LOG_CHANNEL", default=-1002674737457, cast=int)
     CLOUD_CHANNEL = config("CLOUD_CHANNEL", default=-100235858844, cast=int)
-   FORCESUB_CHANNEL = config(
-    "FORCESUB_CHANNEL",
-    default="-1002656761774 -1002358532189 -1001526632511 -1001657207796",
-    cast=lambda v: [int(x) for x in v.split()]
-)
+    FORCESUB_CHANNEL = config(
+        "FORCESUB_CHANNEL",
+        default="-1002656761774 -1002358532189 -1001526632511 -1001657207796",
+        cast=lambda v: [int(x) for x in v.split()]
+    )
     OWNER = config("OWNER", default=1077880102, cast=int)
 
     # Other Configs
-
-    THUMB = config(
-        "THUMBNAIL", default="https://envs.sh/s/6mkCjdFJApbEYZX8stsBuA/r4q.jpg"
-    )
+    THUMB = config("THUMBNAIL", default="https://envs.sh/s/6mkCjdFJApbEYZX8stsBuA/r4q.jpg")
     FFMPEG = config("FFMPEG", default="ffmpeg")
     CRF = config("CRF", default="25")
     SEND_SCHEDULE = config("SEND_SCHEDULE", default=True, cast=bool)
     RESTART_EVERDAY = config("RESTART_EVERDAY", default=True, cast=bool)
     LOG_ON_MAIN = config("LOG_ON_MAIN", default=False, cast=bool)
-    FORCESUB_CHANNEL = config(
-    "FORCESUB_CHANNEL",
-    default="-1002656761774 -1002358532189 -1001526632511 -1001657207796",
-    cast=lambda v: [int(x) for x in v.split()]
-)
-    # Dev Configs
 
+    # Dev Configs
     DEV_MODE = config("DEV_MODE", default=False, cast=bool)
