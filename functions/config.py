8fab9542085fbccbccdd87e9f483ebee0c783cf4
MinuteCodingsTelugu <156009168+MinuteCodingsTelugu@users.noreply.github.com>
2025-05-24 19:12:10 +0530

    Update config.py

--- functions/config.py
+++ functions/config.py
@@ -37,7 +37,7 @@ class Var:
     MAIN_CHANNEL = config("MAIN_CHANNEL", default=-1002335038787, cast=int)
     LOG_CHANNEL = config("LOG_CHANNEL", default=-1002674737457, cast=int)
     CLOUD_CHANNEL = config("CLOUD_CHANNEL", default=-100235858844, cast=int)
-    FORCESUB_CHANNEL = config("FORCESUB_CHANNEL", default=0, cast=int)
+    FORCESUB_CHANNEL = config("FORCESUB_CHANNEL", default=-1002358532189, cast=int)
     OWNER = config("OWNER", default=1077880102, cast=int)
 
     # Other Configs
@@ -47,7 +47,7 @@ class Var:
     SEND_SCHEDULE = config("SEND_SCHEDULE", default=True, cast=bool)
     RESTART_EVERDAY = config("RESTART_EVERDAY", default=True, cast=bool)
     LOG_ON_MAIN = config("LOG_ON_MAIN", default=False, cast=bool)
-    FORCESUB_CHANNEL_LINK = config("FORCESUB_CHANNEL_LINK", default="", cast=str)
+    FORCESUB_CHANNEL_LINK = config("FORCESUB_CHANNEL_LINK", default="https://t.me/Animes2u", cast=str)
 
     # Dev Configs
     DEV_MODE = config("DEV_MODE", default=False, cast=bool)
